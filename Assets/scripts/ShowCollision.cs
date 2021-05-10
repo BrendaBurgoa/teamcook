dada24e0f024e23a223a08f78881fce08a462bd9
Agustin Pontura <pontura@gmail.com>
2021-05-10 15:51:13 -0300

    fires

--- Assets/scripts/ShowCollision.cs
+++ Assets/scripts/ShowCollision.cs
@@ -10,39 +10,37 @@ public Material[] shownMat;
 public Color matcolor;
 private bool grabbed;
 private GameObject character;
+
     void Start () {
-        //reconoce los materiales del objeto
         ren = gameObject.GetComponent<Renderer>();
-        mat = ren.materials;
-       
+        mat = ren.materials;       
         matcolor =mat[0].color;
     }
     void Update(){
-        if (gameObject.GetComponent<pick_drop>() != null && transform.parent != null){
-            //si tiene padre y no clase para ser agarrado (elementos chopped) se chequea que el padre sea un personaje y se vuelven a poner los materiales originales 
+        if (gameObject.GetComponent<pick_drop>() != null && transform.parent != null){            
             if (transform.parent.tag == "destination"){
-                    shownMat=ren.materials;
-                    shownMat[0].color = matcolor;
-                    ren.materials=shownMat;
+                SetColor(matcolor);
             }
         }
     }
     void OnCollisionEnter(Collision other){
          if (other.gameObject.tag == "character"){
-             //si colisiona con un personaje se cambia el color del material por verde
-                shownMat=ren.materials;
-                shownMat[0].color = Color.green;
-                ren.materials=shownMat;
-                character = other.gameObject;
+            SetColor(Color.green);
+            character = other.gameObject;
          }
      }
     void OnCollisionExit(Collision other){
-        //si un personaje se aleja se retoman los materiales originales
         if (other.gameObject.tag == "character"){
-                shownMat=ren.materials;
-                shownMat[0].color = matcolor;
-                ren.materials=shownMat;
+            if (character != null && character != other.gameObject) return;
+            character = null;
+            SetColor(matcolor);
         }
+    }
+    void SetColor(Color matcolor)
+    {
+        shownMat = ren.materials;
+        shownMat[0].color = matcolor;
+        ren.materials = shownMat;
     }
 
 }
--- Assets/scripts/fires.cs
+++ Assets/scripts/fires.cs
@@ -16,6 +16,7 @@ public class fires : Photon.MonoBehaviour
     private void instantiateFires()
     {
         Vector3 position = new Vector3(Random.Range(-3f, 3.0f), 0, Random.Range(0f, 4.0f));
-        PhotonNetwork.Instantiate(myPrefab.name, position, Quaternion.identity, 0); 
+        //  PhotonNetwork.Instantiate(myPrefab.name, position, Quaternion.identity, 0); 
+        Instantiate(myPrefab, position, Quaternion.identity);
     }
 }
